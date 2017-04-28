144d7609c33995ba5be46080e81f2048427ce93a
Wesley <wesleymcox@gmail.com>
2017-04-27 22:06:16 -0700

    Controls Poller method stubs and domain

---
+++ source/controls/ControlsInput.hx
@@ -0,0 +1,17 @@
+package controls;
+import domain.VerticalDisplacement;
+import domain.HorizontalDisplacement;
+
+/**
+ * Represents all of a player's current controller input
+**/
+class ControlsInput {
+
+    public var horizontalDisplacement : HorizontalDisplacement;
+    public var verticalDisplacement : VerticalDisplacement;
+
+    public function new(horizontal : HorizontalDisplacement, vertical : VerticalDisplacement) {
+        this.horizontalDisplacement = horizontal;
+        this.verticalDisplacement = vertical;
+    }
+}
---
+++ source/controls/ControlsPoller.hx
@@ -0,0 +1,13 @@
+package controls;
+import flixel.FlxG;
+class ControlsPoller {
+
+    public function new() {
+
+    }
+
+    public function getMovementDirection():ControlsInput {
+        // TODO
+        return null;
+    }
+}
