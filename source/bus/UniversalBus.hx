1592cf9ff151189f445f33b56d14ea1f295c302c
Anthony Ha <anthonyha96@gmail.com>
2017-04-29 13:59:54 -0700

    Create UniversalBus which will hold all buses

---
+++ source/bus/UniversalBus.hx
@@ -0,0 +1,8 @@
+package bus;
+
+class UniversalBus {
+    // Add different bus as properties to this class. See below for an example
+    // public var intEvents(default, null):Bus<Int> = new Bus<Int>();
+    public function new() {
+    }
+}
