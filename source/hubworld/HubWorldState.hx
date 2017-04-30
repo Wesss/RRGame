76de2c88d8f8263c05d5b8bdc0a65835c40e736a
Wesley <wesleymcox@gmail.com>
2017-04-29 20:00:15 -0700

    created start menu, hubworld, and level playstate stubs

---
+++ source/hubworld/HubWorldState.hx
@@ -0,0 +1,16 @@
+package hubworld;
+
+import flixel.FlxState;
+
+class HubWorldState extends FlxState
+{
+    override public function create():Void
+    {
+        super.create();
+    }
+
+    override public function update(elapsed:Float):Void
+    {
+        super.update(elapsed);
+    }
+}
---
+++ source/start_menu/StartMenuState.hx
@@ -0,0 +1,19 @@
+package start_menu;
+
+import flixel.FlxState;
+
+/**
+ * This is a FlxState that represents the first menu the player is taken to upon starting the game.
+ **/
+class StartMenuState extends FlxState
+{
+    override public function create():Void
+    {
+        super.create();
+    }
+
+    override public function update(elapsed:Float):Void
+    {
+        super.update(elapsed);
+    }
+}
