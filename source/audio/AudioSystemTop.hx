be1d998d1f574594cb75420a6c51d6ab8fb246c4
Wesley <wesleymcox@gmail.com>
2017-05-02 22:31:40 -0700

    audio system plays a 'woosh' on player move, assets seperated into used and unused

--- source/audio/AudioSystemTop.hx
+++ source/audio/AudioSystemTop.hx
@@ -1,13 +1,22 @@
 package audio;
 
+import flixel.FlxG;
+import domain.Displacement;
 import bus.UniversalBus;
 
 /**
- * Top level module for controlling audio. Responsible for loading/playing of sounds at precise times.
- * This class is NOT responsible for any sort of timing coordination
+ * Top level module for controlling audio. Responsible for loading/playing of sounds.
+ * This class is NOT responsible for any sort of precise timing or coordination
  **/
 class AudioSystemTop {
-    public function new(universalBus:UniversalBus) {
 
+    private var moveSound = FlxG.sound.load(AssetPaths.NFFsquirt02__wav);
+
+    public function new(universalBus:UniversalBus) {
+        universalBus.controlsEvents.subscribe(playMovementSounds);
+    }
+
+    public function playMovementSounds(event:Displacement):Void {
+        moveSound.play(false, 0, 50);
     }
 }
