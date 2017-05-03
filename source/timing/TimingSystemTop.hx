81afebef06a629a885e5825c6ca646a056233ffd
Wesley <wesleymcox@gmail.com>
2017-05-03 14:27:33 -0700

    timing system preliminary method stubs

--- source/timing/TimingSystemTop.hx
+++ source/timing/TimingSystemTop.hx
@@ -15,4 +15,25 @@ class TimingSystemTop {
         beatEventBus = universalBus.beatEvents;
         // TODO hook up to bus once level system is more fleshed out
     }
+
+    /**
+     * Start releasing beat events as a song starts playing
+     **/
+    public function trackSongStart() {
+
+    }
+
+    /**
+     * If a song is playing, update our time and release beat events as appropriate
+     **/
+    public function update() {
+
+    }
+
+    /**
+     * re-align the timing when we know for sure our place in the music
+     **/
+    public function updateMusicPlayhead() {
+
+    }
 }
