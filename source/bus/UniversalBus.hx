959c2017dce8bcbd15363d17f24b5bfb8054fe03
Wesley <wesleymcox@gmail.com>
2017-05-02 21:21:59 -0700

    sound effect asset mass upload

--- source/bus/UniversalBus.hx
+++ source/bus/UniversalBus.hx
@@ -6,14 +6,9 @@ import domain.Displacement;
  *  UniversalBus is a collection of different buses that are going to be used.
  *  The different buses are exposed via read-only properties of an instance of this class.
  */
-import audio.AudioEvent;
 class UniversalBus {
-    // Add different bus as properties to this class. See below for an example
-
-    public var audioEvents(default, null):Bus<AudioEvent> = new Bus<AudioEvent>();
 
     public var controlsEvents(default, null):Bus<Displacement> = new Bus<Displacement>();
 
-    public function new() {
-    }
+    public function new() {}
 }
