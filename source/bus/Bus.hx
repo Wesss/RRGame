6edb18278202847079ec7a608bf742f9c270ed03
Anthony Ha <anthonyha96@gmail.com>
2017-05-03 22:21:01 -0700

    Have bus take the subscriber object to allow unsubscription

--- source/bus/Bus.hx
+++ source/bus/Bus.hx
@@ -5,17 +5,25 @@ package bus;
  */
 @:generic
 class Bus<T> {
-    var subscribers = new Array<(T) -> Void>();
+    var subscribers = new Map<{}, (T) -> Void>();
 
     public function new() {
     }
     
     /**
-     *  Subscribes the given function to this bus.
-     *  Subscribers will be called for each message broadcasted on this bus.
+     *  Subscribes the given subscriber to this bus
+     *  Subscribers will be called through the callback for each message broadcasted on this bus.
      */
-    public function subscribe(receiver : (T) -> Void) {
-        subscribers.push(receiver);
+    public function subscribe(subscriber, callback : (T) -> Void) {
+        subscribers[subscriber] = callback;
+    }
+
+    /**
+     *  Unsubscribes the given subscriber from this bus
+     *  The given subscriber won't be notified of events broadcasted on this bus anymore.
+     */
+    public function unsubscribe(subscriber) {
+        subscribers.remove(subscriber);
     }
 
     /**
