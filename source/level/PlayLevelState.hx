5fa9cc8558bdb1d5b35269548673c137032851a5
Anthony Ha <anthonyha96@gmail.com>
2017-05-04 20:45:35 -0700

    Clean up PlayLevelState using imports

--- source/level/PlayLevelState.hx
+++ source/level/PlayLevelState.hx
@@ -4,6 +4,7 @@ import board.BoardSystemTop;
 import controls.ControlsSystemTop;
 import flixel.FlxState;
 import flixel.FlxG;
+import flixel.math.FlxPoint;
 
 class PlayLevelState extends FlxState
 {
@@ -14,7 +15,7 @@ class PlayLevelState extends FlxState
 		add(new ControlsSystemTop(unibus));
 		add(new BoardSystemTop(0, 0, unibus));
 
-		FlxG.camera.focusOn(new flixel.math.FlxPoint(0, 0));
+		FlxG.camera.focusOn(new FlxPoint(0, 0));
 		unibus.playerMoved.subscribe(this, function(displacement) {FlxG.camera.shake(0.01, 0.1);});
 
 		super.create();
