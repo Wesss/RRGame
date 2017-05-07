a0047fcba868dfdc4909dbbbc73b8963880b9ee8
Wesley <wesleymcox@gmail.com>
2017-05-07 02:24:37 -0700

    track actions mock creation to prove unit test works

--- source/level/LevelDataLoader.hx
+++ source/level/LevelDataLoader.hx
@@ -1,5 +1,9 @@
 package level;
 
+import domain.Displacement;
+import bus.UniversalBus;
+import track_action.SliderThreat;
+import track_action.TrackAction;
 import flixel.addons.editors.ogmo.FlxOgmoLoader;
 
 /**
@@ -13,6 +17,19 @@ class LevelDataLoader {
         var musicAssetPath = "assets/music/" + loader.getProperty("MusicTrack");
         var bpm = Std.parseInt(loader.getProperty("BPM"));
         var offset = Std.parseInt(loader.getProperty("MusicStartOffset"));
-        return new LevelData(musicAssetPath, bpm, offset, null);
+
+        var trackActions = new Array<TrackAction>();
+        var mockBus = new UniversalBus();
+        // TODO DELETE THIS. Just a mock correct answer to show test passes/fails properly
+//        trackActions.push(new SliderThreat(0.0, 135, new Displacement(LEFT, UP), mockBus));
+//        trackActions.push(new SliderThreat(4.0, 135, new Displacement(LEFT, NONE), mockBus));
+//        trackActions.push(new SliderThreat(8.0, 135, new Displacement(LEFT, DOWN), mockBus));
+//        trackActions.push(new SliderThreat(12.0, 135, new Displacement(NONE, UP), mockBus));
+//        trackActions.push(new SliderThreat(12.0, 135, new Displacement(NONE, NONE), mockBus));
+//        trackActions.push(new SliderThreat(12.0, 135, new Displacement(NONE, DOWN), mockBus));
+//        trackActions.push(new SliderThreat(16.0, 135, new Displacement(RIGHT, UP), mockBus));
+//        trackActions.push(new SliderThreat(16.0, 135, new Displacement(RIGHT, NONE), mockBus));
+//        trackActions.push(new SliderThreat(16.0, 135, new Displacement(RIGHT, DOWN), mockBus));
+        return new LevelData(musicAssetPath, bpm, offset, trackActions);
     }
 }
