a9d5060f4588295b07a5d33dfb0e752a48497c23
Anthony Ha <anthonyha96@gmail.com>
2017-05-08 09:52:26 -0700

    Refactor lockable level buttons

--- source/hubworld/WorldSpriteGroup.hx
+++ source/hubworld/WorldSpriteGroup.hx
@@ -19,27 +19,51 @@ class WorldSpriteGroup extends FlxSpriteGroup {
 
         for (i in 0...world.levels.length) {
             var location = hubWorldData.buttonLocations[i];
-            var button = new FlxButton(location.x, location.y, "" + (i + 1));
-            button.loadGraphic(AssetPaths.BoardSquare__png);
-            button.label.setFormat(AssetPaths.GlacialIndifference_Regular__ttf,
-                50, flixel.util.FlxColor.WHITE, CENTER);
-            var centerOffset = new FlxPoint(0, button.height / 2 - button.label.height / 2 - 5);
-            button.labelOffsets = [centerOffset, centerOffset, centerOffset];
+            var button = new SelectLevelButton(location.x, location.y, i + 1 + "", world.levels[i], !tutorialPassed && i > 0);
             add(button);
-            
-            if (!tutorialPassed && i > 0) {
-                var lockOverlay = new FlxSprite(location.x, location.y, AssetPaths.LockOverlay__png);
-                add(lockOverlay);
+        }
+    }
+}
 
-                button.onDown.callback = null;
-            } else {
-                button.onDown.callback = function() {
-                    trace("Start level: " + (i + 1));
-                    var universalBus = new UniversalBus();
-                    var levelData = LevelDataLoader.loadLevelData(world.levels[i], universalBus);
-                    FlxG.switchState(new PlayLevelState(levelData, new FlxSpriteGroup(), universalBus));
-                }
+class SelectLevelButton extends FlxSpriteGroup {
+    private var isLocked : Bool;
+    private var lockOverlay : FlxSprite;
+
+    public function new(x : Float, y : Float, label : String, levelAssetPath : String, isLocked = false) {
+        super(x, y);
+        var button = new FlxButton(0, 0, label);
+        button.loadGraphic(AssetPaths.BoardSquare__png);
+        button.label.setFormat(AssetPaths.GlacialIndifference_Regular__ttf,
+                50, flixel.util.FlxColor.WHITE, CENTER);
+        var centerOffset = new FlxPoint(0, button.height / 2 - button.label.height / 2 - 5);
+        button.labelOffsets = [centerOffset, centerOffset, centerOffset];
+        add(button);
+
+        lockOverlay = new FlxSprite(0, 0, AssetPaths.LockOverlay__png);
+        add(lockOverlay);
+
+        button.onDown.callback = function() {
+            if (!isLocked) {
+                trace("Starting level: " + levelAssetPath);
+
+                var universalBus = new UniversalBus();
+                var levelData = LevelDataLoader.loadLevelData(levelAssetPath, universalBus);
+                FlxG.switchState(new PlayLevelState(levelData, new FlxSpriteGroup(), universalBus));
             }
         }
+
+        if (!isLocked) {
+            lockOverlay.alpha = 0;
+        }
+    }
+
+    public function lock() {
+        isLocked = true;
+        lockOverlay.alpha = 1;
+    }
+
+    public function unlock() {
+        isLocked = false;
+        lockOverlay.alpha = 0;
     }
 }
