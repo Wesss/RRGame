222c48de044f185d32f7f760022c0f64301733d1
Wesley <wesleymcox@gmail.com>
2017-05-10 22:27:30 -0700

    saves only run when running in js

--- source/hubworld/SaveManager.hx
+++ source/hubworld/SaveManager.hx
@@ -7,6 +7,7 @@ import js.Browser;
 class SaveManager {
 
     public static function initializeSaveData() {
+        #if js
         var localStorage = Browser.window.localStorage;
 
         if (localStorage.getItem("isInitiallized") != null) {
@@ -14,16 +15,21 @@ class SaveManager {
         }
         saveProgress(new Map<Int, Float>());
         localStorage.setItem("isInitiallized", "true");
+        #end
     }
 
     public static function getProgress():Map<Int, Float> {
+        #if js
         var unserializer = new Unserializer(Browser.window.localStorage.getItem("levelScore"));
         return unserializer.unserialize();
+        #end
     }
 
     public static function saveProgress(progress:Map<Int, Float>):Void {
+        #if js
         var serializer = new Serializer();
         serializer.serialize(progress);
         Browser.window.localStorage.setItem("levelScore", serializer.toString());
+        #end
     }
 }
