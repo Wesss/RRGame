a8015ad4200e4c95ef6bb77956ee264116a8e2ae
Wesley <wesleymcox@gmail.com>
2017-05-12 12:29:31 -0700

    method/class skeletons

---
+++ source/pause_options_menu/PauseOptionsMenu.hx
@@ -0,0 +1,19 @@
+package pause_options_menu;
+
+import flixel.FlxSubState;
+
+/**
+ * A menu that pauses the game and allows for manipulating persistent options settings
+ **/
+class PauseOptionsMenu extends FlxSubState {
+
+    // TODO potentially use flixel.addons.ui.FlxSlider
+
+    public function new() {
+        super();
+    }
+
+    override public function create():Void {
+        super.create();
+    }
+}
