ec041b7b5cb0002c8529fdc8b4a3f58abf326432
wesley <wesleymcox@gmail.com>
2017-05-15 14:23:21 -0700

    level3 skeleton setup

--- manual_test/runthrough_actual_level/RunActualLevelTest.hx
+++ manual_test/runthrough_actual_level/RunActualLevelTest.hx
@@ -13,7 +13,7 @@ import flixel.FlxState;
 class RunActualLevelTest extends FlxState {
 
 	// change this to change the level run
-	private static inline var LEVEL_ASSET_PATH = "assets/levels/level0.oel";
+	private static inline var LEVEL_ASSET_PATH = "assets/levels/level3.oel";
 
 	override public function create():Void {
 		super.create();
