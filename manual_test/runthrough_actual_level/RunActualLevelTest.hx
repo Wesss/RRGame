1990b78e2eeb9e1ff84dc8af0789198047708ba4
wesley <wesleymcox@gmail.com>
2017-05-19 00:23:01 -0700

    level 8 skeleton

--- manual_test/runthrough_actual_level/RunActualLevelTest.hx
+++ manual_test/runthrough_actual_level/RunActualLevelTest.hx
@@ -13,7 +13,7 @@ import flixel.FlxState;
 class RunActualLevelTest extends FlxState {
 
 	// change this to change the level run
-	private static inline var LEVEL_ASSET_PATH = "assets/levels/level7.oel";
+	private static inline var LEVEL_ASSET_PATH = "assets/levels/level8.oel";
 
 	override public function create():Void {
 		super.create();
