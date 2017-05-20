1745ea9e23d088cdc27bbc9d44abc4f497023855
Wesley <wesleymcox@gmail.com>
2017-05-20 16:38:48 -0700

    progress bar stops on death

--- source/level/ProgressBar.hx
+++ source/level/ProgressBar.hx
@@ -11,30 +11,39 @@ class ProgressBar extends FlxText {
     private static inline var Y_POSITION = 200;
     private var userInterfaceGroup:FlxSpriteGroup;
     private var levelEndBeat:Float;
+    private var isProgressing:Bool;
 
     public function new(universalBus:UniversalBus, userInterfaceGroup) {
-        this.userInterfaceGroup = userInterfaceGroup;
         super(X_POSITION, Y_POSITION, "");
+        this.userInterfaceGroup = userInterfaceGroup;
         setFormat(AssetPaths.GlacialIndifference_Regular__ttf, 16, flixel.util.FlxColor.WHITE, CENTER);
 
         universalBus.levelStart.subscribe(this, levelStart);
         universalBus.beat.subscribe(this, updateBeat);
+        universalBus.playerDie.subscribe(this, stopProgressing);
     }
 
     public function levelStart(event:LevelStartEvent) {
         this.levelEndBeat = event.lastBeat;
         this.text = "0% complete";
+        this.isProgressing = true;
         userInterfaceGroup.add(this);
     }
 
     public function updateBeat(event:BeatEvent) {
-        var completion = Std.int((event.beat / levelEndBeat) * 100);
-        if (completion < 0) {
-            completion = 0;
+        if (isProgressing) {
+            var completion = Std.int((event.beat / levelEndBeat) * 100);
+            if (completion < 0) {
+                completion = 0;
+            }
+            if (completion > 100) {
+                completion = 100;
+            }
+            this.text =  completion + "% complete";
         }
-        if (completion > 100) {
-            completion = 100;
-        }
-        this.text =  completion + "% complete";
+    }
+
+    public function stopProgressing(_) {
+        isProgressing = false;
     }
 }
