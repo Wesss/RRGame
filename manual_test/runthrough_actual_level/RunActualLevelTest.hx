f0d14ff5d4976131739622774ec0cb250aad8f93
wesley <wesleymcox@gmail.com>
2017-07-05 00:01:24 -0700

    filled in hard level

--- manual_test/runthrough_actual_level/RunActualLevelTest.hx
+++ manual_test/runthrough_actual_level/RunActualLevelTest.hx
@@ -13,7 +13,7 @@ import flixel.FlxState;
 class RunActualLevelTest extends FlxState {
 
 	// change this to change the level run
-	private static inline var LEVEL_ASSET_PATH = "assets/levels/normal.oel";
+	private static inline var LEVEL_ASSET_PATH = "assets/levels/hard.oel";
 
 	override public function create():Void {
 		super.create();
